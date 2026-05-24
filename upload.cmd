aa89e62bbda2786d6d981877fff5518ee4f3f242
pe5jw <120334387+pe5jw@users.noreply.github.com>
2026-05-24 17:20:11 +0200

    Delete upload.cmd

--- upload.cmd
+++
@@ -1,7 +0,0 @@
-cd C:\github\not1mm-windows-installer
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote add origin https://github.com/PE5JW/not1mm-windows-installer.git
-git push -u origin main
